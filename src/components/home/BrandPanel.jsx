0fa63951d44c35b9a15bfdf23cde7fbbf4dc9b19
Sahil Bijlani <bijlanisahil511@gmail.com>
2026-08-22 13:18:17 +0530

    fix: changes logo extenstion from png to jpeg

--- src/components/home/BrandPanel.jsx
+++ src/components/home/BrandPanel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo.jpeg";
 
 export default function BrandPanel({ mode = "borrower" }) {
   const isPartner = mode === "partner";
