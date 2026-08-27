58edc928c8b372bf247379f65a3f3289d6203add
Sahil Bijlani <bijlanisahil511@gmail.com>
2026-08-27 16:47:09 +0530

    fix: update header phone number to 1800-123-4567

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -63,11 +63,11 @@ export default function Navbar() {
           </div>
         </Link>
         <div className="nav-top-right">
-          <a className="nav-phone" href="tel:9910507574">
+          <a className="nav-phone" href="tel:18001234567">
             <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2A19.79 19.79 0 0 1 3.08 4.18 2 2 0 0 1 5.07 2h3a2 2 0 0 1 2 1.72c.12.96.36 1.9.7 2.81a2 2 0 0 1-.45 2.11L9.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.9.34 1.85.58 2.81.7A2 2 0 0 1 22 16.92z"/>
             </svg>
-            99105 07574
+            1800-123-4567
           </a>
           {isAuthenticated ? (
             <button className="nav-cta" onClick={handleLogout}>Sign Out</button>
