639c6f861db19b5ac70de7cf1a597c0775c03794
Sneha Dewani <snehadewani07@gmail.com>
2026-09-12 13:37:01 +0530

    remove company ribbon logo and tagline entirely

--- src/components/home/CompanyRibbon.jsx
+++ src/components/home/CompanyRibbon.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import logo from "../../assets/images/brandlogo.png";
 
 export default function CompanyRibbon() {
-  return (
-    <div className="company-ribbon">
-      <div className="company-ribbon-name">
-        <img
-          src={logo}
-          className="company-ribbon-logo"
-          alt="Finn4sure Logo"
-        />
-      </div>
-    </div>
-  );
+  return null;
 }
 export { CompanyRibbon };
